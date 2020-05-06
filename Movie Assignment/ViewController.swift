b0950b59cd35a2f424520f74c263397d2a9303de
bako abdullah <bako.abdullah24@gmail.com>
2020-05-06 21:55:12 +0300

    Initial Commit

---
+++ Movie Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Assignment
+//
+//  Created by bako abdullah on 5/6/20.
+//  Copyright © 2020 bako abdullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
